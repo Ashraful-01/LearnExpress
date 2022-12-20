82aea927d53278af6623eacfb3352ee0b8c8c97c
Ashraful Alam <mdmashraful1998@gmail.com>
2022-12-21 01:32:41 +0600

    data call from server by using get method

--- app.js
+++ app.js
@@ -1,67 +1,27 @@
 //express 
-
 const express = require('express');
-
 const app = express();
+const fs = require('fs');
 
 app.get('/', (req, res) => {
     res.send("Hi There!, I am from Express Js");
 })
 
+app.get('/ashraf', (req, res) => {
+    res.send("He is a Cool Programmer");
+});
 
+
+app.get('/students', (req, res) => {
+    fs.readFile('./db.json', 'utf-8', (err, data) => {
+        console.log(data);
+        const students = JSON.parse(data).students;
+        res.send(students);
+    });
+});
 
 app.listen(5000);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const { response } = require('express');
-// const express = require('express');
-
-// const app = express();
-
-// app.get('/', (request, response) => {
-//     response.send("hello from express js");
-// });
-
-// app.get('/another', (request, response) => {
-//     response.send("I am from another!");
-// })
-// app.get('/students', (req, res) => {
-//     res.send(JSON.stringify(["Rahim", "Karim"]));
-// });
-
-// const port = 3000;
-
-// app.listen(port, () => {
-//     console.log(`Listening on port 3000....`);
-// })
 
 
